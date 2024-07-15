70b1f5ebccd4ea2f90e639e667deb6808a70c528
Shane <6071159+smashedr@users.noreply.github.com>
2024-07-15 02:59:49 -0700

    Add Client and Variables

--- src/index.js
+++ src/index.js
@@ -1,30 +1,58 @@
 const core = require('@actions/core')
-// const github = require('@actions/github')
+const github = require('@actions/github')
 const Portainer = require('./portainer')
 
 ;(async () => {
     try {
+        console.log('github.context:', github.context)
+        const { owner, repo } = github.context.repo
+        console.log('owner:', owner)
+        console.log('repo:', repo)
+        const repositoryURL = `https://github.com/${owner}/${repo}`
+        console.log('repositoryURL:', repositoryURL)
+
         const url = core.getInput('url', { required: true })
         console.log('url:', url)
         const token = core.getInput('token', { required: true })
         console.log('token:', token)
+        let endpoint = core.getInput('endpoint')
+        console.log('endpoint:', endpoint)
+        const name = core.getInput('name', { required: true })
+        console.log('name:', name)
+        const file = core.getInput('file', { required: true })
+        console.log('file:', file)
 
         const portainer = new Portainer(url, token)
 
-        const endpoints = await portainer.getEndpoints()
-        // console.log('endpoints:', endpoints)
-
-        const endpoint = endpoints[0].Id
-        console.log('endpoint:', endpoint)
+        if (!endpoint) {
+            const endpoints = await portainer.getEndpoints()
+            // console.log('endpoints:', endpoints)
+            endpoint = endpoints[0].Id
+            console.log('endpoint:', endpoint)
+        }
 
         const swarm = await portainer.getSwarm(endpoint)
         // console.log('swarm:', swarm)
-
         const swarmID = swarm.ID
         console.log('swarmID:', swarmID)
 
-        // const stacks = await portainer.getStacks()
+        const stacks = await portainer.getStacks()
         // console.log('stacks:', stacks)
+        console.log('stacks.length:', stacks.length)
+
+        // const stack = portainer.createStack({
+        //     composeFile: file,
+        //     fromAppTemplate: false,
+        //     name,
+        //     repositoryURL,
+        //     repositoryAuthentication: true,
+        //     repositoryUsername: 'myGitUsername',
+        //     repositoryPassword: 'myGitPassword',
+        //     repositoryReferenceName: 'refs/heads/master',
+        //     swarmID,
+        //     tlsskipVerify: false,
+        // })
+        // console.log('stack:', stack)
 
         console.log('+++ SUCCESS ++++')
     } catch (e) {
--- src/portainer.js
+++ src/portainer.js
@@ -3,72 +3,44 @@ const https = require('https')
 
 class Portainer {
     constructor(url, token) {
-        console.log('token:', token)
         url = url.replace(/\/$/, '')
         if (!url.endsWith('api')) {
             url += '/api'
         }
-        this.url = url
-        console.log('this.url:', this.url)
-        this.agent = new https.Agent({
+        const agent = new https.Agent({
             rejectUnauthorized: false,
         })
-        this.headers = {
-            'X-API-Key': token,
-        }
+        this.headers = this.client = axios.create({
+            baseURL: url,
+            headers: {
+                'X-API-Key': token,
+            },
+            httpsAgent: agent,
+        })
     }
 
     async getEndpoints() {
-        const url = `${this.url}/endpoints`
-        console.log('getEndpoints:', url)
-        const response = await axios.get(url, {
-            headers: this.headers,
-            httpsAgent: this.agent,
-        })
+        const response = await this.client.get('/endpoints')
         return response.data
     }
 
     async getSwarm(id) {
-        const url = `${this.url}/endpoints/${id}/docker/swarm`
-        console.log('getSwarm:', url)
-        const response = await axios.get(url, {
-            headers: this.headers,
-            httpsAgent: this.agent,
-        })
+        const response = await this.client.get(`/endpoints/${id}/docker/swarm`)
         return response.data
     }
 
     async getStacks() {
-        const url = `${this.url}/stacks`
-        console.log('getStacks:', url)
-        const response = await axios.get(url, {
-            headers: this.headers,
-            httpsAgent: this.agent,
-        })
+        const response = await this.client.get('/stacks')
         return response.data
     }
 
-    // async createStacks() {
-    //     const url = `${this.url}/stacks/create/swarm/repository`
-    //     console.log('createStacks:', url)
-    //     const body = {
-    //         composeFile: 'docker-compose.yml',
-    //         fromAppTemplate: false,
-    //         name: 'myStack',
-    //         repositoryURL: 'https://github.com/openfaas/faas',
-    //         repositoryAuthentication: true,
-    //         repositoryUsername: 'myGitUsername',
-    //         repositoryPassword: 'myGitPassword',
-    //         repositoryReferenceName: 'refs/heads/master',
-    //         swarmID: 'jpofkc0i9uo9wtx1zesuk649w',
-    //         tlsskipVerify: false,
-    //     }
-    //     const response = await axios.get(url, {
-    //         headers: this.headers,
-    //         httpsAgent: this.agent,
-    //     })
-    //     return response.data
-    // }
+    async createStack(body) {
+        const response = await this.client.post(
+            '/stacks/create/swarm/repository',
+            body
+        )
+        return response.data
+    }
 }
 
 module.exports = Portainer
