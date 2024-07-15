f80acedce1aa3e15943555cc9328bb2a5edd67ff
Shane <6071159+smashedr@users.noreply.github.com>
2024-07-15 01:02:53 -0700

    Update URL

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-// import * as core from '@actions/core'
-// import * as github from '@actions/github'
-
 const core = require('@actions/core')
 // const github = require('@actions/github')
 const Portainer = require('./portainer')
@@ -12,9 +9,9 @@ const Portainer = require('./portainer')
         const token = core.getInput('token', { required: true })
         console.log('token:', token)
 
-        const portainerInstance = new Portainer(url, token)
+        const portainer = new Portainer(url, token)
 
-        const stacks = await portainerInstance.getStacks()
+        const stacks = await portainer.getStacks()
         console.log('Stacks:', stacks)
 
         console.log('+++ SUCCESS ++++')
--- src/portainer.js
+++ src/portainer.js
@@ -1,12 +1,15 @@
-// import axios from 'axios'
-// import https from 'https'
-
 const axios = require('axios')
 const https = require('https')
 
 class Portainer {
     constructor(url, token) {
+        console.log('token:', token)
+        url = url.replace(/\/$/, '')
+        if (!url.endsWith('api')) {
+            url += '/api'
+        }
         this.url = url
+        console.log('this.url:', this.url)
         this.agent = new https.Agent({
             rejectUnauthorized: false,
         })
@@ -16,7 +19,7 @@ class Portainer {
     }
 
     async getStacks() {
-        const url = `${this.url}/api/stacks`
+        const url = `${this.url}/stacks`
         console.log('getStacks:', url)
         const response = await axios.get(url, {
             headers: this.headers,
@@ -27,18 +30,3 @@ class Portainer {
 }
 
 module.exports = Portainer
-
-// export async function getStacks(url, token) {
-//     url = url + '/api/stacks'
-//     console.log('getStacks:', url, token)
-//     const agent = new https.Agent({
-//         rejectUnauthorized: false,
-//     })
-//     const response = await axios.get(url, {
-//         headers: {
-//             'X-API-Key': token,
-//         },
-//         httpsAgent: agent,
-//     })
-//     return response.data
-// }
